553f77534fd69dd47d2c94f27ee7a4a8b5d4700f
EC2 Default User <ec2-user@ip-172-31-13-171.us-west-1.compute.internal>
2022-07-13 12:35:30 +0000

    インデント数、再スタートの活性

--- stop_watch.js
+++ stop_watch.js
@@ -1,13 +1,13 @@
-const timer = document.getElementById('timer');
-const start = document.getElementById('start');
-const stop = document.getElementById('stop');
-const reset = document.getElementById('reset');
+  const timer = document.getElementById('timer');
+  const start = document.getElementById('start');
+  const stop = document.getElementById('stop');
+  const reset = document.getElementById('reset');
 
-let startTime;
-let timeOutId;
-let elapsedTime = 0;
+  let startTime;
+  let timeOutId;
+  let elapsedTime = 0;
 
-function countUp (){
+  function countUp (){
     
     const d = new Date(Date.now()- startTime + elapsedTime);
     const h = String(d.getUTCHours()).padStart(1,'0');
@@ -15,63 +15,63 @@ function countUp (){
     const s =String(d.getSeconds()).padStart(1,'0');
     const ms =String(d.getMilliseconds()).slice(0,1);
 
-    timer.textContent = `${h} : ${m} : ${s} : ${ms}`;
+    timer.textContent = `${h}:${m}:${s}:${ms}`;
     
     timeOutId = setTimeout(() => {
         countUp();
     },10);
-    }
+  }
     
-    function buttonInitial (){
-        start.classList.remove('inactive');
-        stop.classList.add('inactive');
-        reset.classList.add('inactive');
-        stop.disabled = true;
-        reset.disabled = true;
-    }
-    function buttonRunning (){
-        start.classList.add('inactive');
-        stop.classList.remove('inactive');
-        reset.classList.add('inactive');
-        start.disabled = true;
-        stop.disabled = false;
-        reset.disabled = true;
-    }
-    function buttonStopped (){
-        start.classList.add('inactive');
-        stop.classList.add('inactive');
-        reset.classList.remove('inactive');
-        start.disabled = true;
-        stop.disabled = true;
-        reset.disabled = false;
-    }
+  function buttonInitial (){
+    start.classList.remove('inactive');
+    stop.classList.add('inactive');
+    reset.classList.add('inactive');
+    stop.disabled = true;
+    reset.disabled = true;
+  }
+  function buttonRunning (){
+    start.classList.add('inactive');
+    stop.classList.remove('inactive');
+    reset.classList.add('inactive');
+    start.disabled = true;
+    stop.disabled = false;
+    reset.disabled = true;
+  }
+  function buttonStopped (){
+    start.classList.remove('inactive');
+    stop.classList.add('inactive');
+    reset.classList.remove('inactive');
+    start.disabled = false;
+    stop.disabled = true;
+    reset.disabled = false;
+  }
     
-    buttonInitial()
+  buttonInitial()
     
-    start.addEventListener('click',() => {
-        if (start.classList.contains('inactive') === true) {
-            return;
-        }
-        buttonRunning();
-        startTime = Date.now();
-        countUp();
+  start.addEventListener('click',() => {
+    if (start.classList.contains('inactive') === true) {
+        return;
+    }
+  buttonRunning();
+    startTime = Date.now();
+    countUp();
+  })
+    
+  stop.addEventListener('click', () => {
+    if (stop.classList.contains('inactive') === true) {
+        return;
+    }
+    buttonStopped();
+    clearTimeout(timeOutId);
+    elapsedTime += Date.now() - startTime;
     })
     
-    stop.addEventListener('click', () => {
-        if (stop.classList.contains('inactive') === true) {
-            return;
-        }
-        buttonStopped();
-        clearTimeout(timeOutId);
-        elapsedTime += Date.now() - startTime;
-    })
-    
-    reset.addEventListener('click', () => {
-        if(reset.classList.contains('inactive') === true) {
-            return;
-        }
-        buttonInitial();
-        timer.textContent = '0:0:0:0';
-        elapsedTime = 0;
-        start.disabled =false;
-    })
+  reset.addEventListener('click', () => {
+    if(reset.classList.contains('inactive') === true) {
+       return;
+    }
+    buttonInitial();
+    timer.textContent = '0:0:0:0';
+    elapsedTime = 0;
+    start.disabled =false;
+  })
